d7792c64a2df67c52e35d448a36ab0f88c4d1fc8
Sylvan Le Deunff <sylvan.le-deunff@ovhcloud.com>
2025-09-25 16:11:27 +0200

    B1

--- foo/db/models.py
+++ foo/db/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
-from sqlalchemy import Table, Integer, String, ForeignKey, Column
+from sqlalchemy import Enum as SQLEnum, Table, Integer, String, ForeignKey, Column
+from enum import Enum
 
 class DBModel(DeclarativeBase): ...
 
@@ -36,4 +37,20 @@ user_groups = Table(
     "user_groups", DBModel.metadata,
     Column("user_id", ForeignKey("users.id"), primary_key=True),
     Column("group_id", ForeignKey("groups.id"), primary_key=True)
+)
+
+
+class UserFriendShipState(Enum):
+    ignored = "ignored"
+    denied = "denied"
+    requested = "requested"
+    pending = "pending"
+    accepted = "accepted"
+
+
+users_friendships = Table(
+    "users_friendships", DBModel.metadata,
+    Column("user_id", ForeignKey("users.id"), primary_key=True),
+    Column("friend_id", ForeignKey("users.id"), primary_key=True),
+    Column("state", SQLEnum(UserFriendShipState), nullable=False),
 )
