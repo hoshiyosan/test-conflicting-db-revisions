dc3e8660b7d895e16d9ee3b17335b7dbee877c64
Sylvan Le Deunff <sylvan.le-deunff@ovhcloud.com>
2025-09-25 15:42:35 +0200

    A1

--- foo/db/api.py
+++ foo/db/api.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from foo.db.models import DBModel
 
-database_url = "sqlite:///db.sqlite"
+environment = input("environment: ")
+
+database_url = f"sqlite:///db.{environment}.sqlite"
 engine = create_engine(database_url)
 session = scoped_session(sessionmaker(bind=engine))
--- foo/db/models.py
+++ foo/db/models.py
@@ -1,4 +1,11 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy import Table, Integer, String, ForeignKey, Column
 
-class DBModel(DeclarativeBase):
-    ...
+class DBModel(DeclarativeBase): ...
+
+
+class DBUser(DBModel):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
