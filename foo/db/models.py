d0e033318cb43a295363f31d4060020508964a62
Sylvan Le Deunff <sylvan.le-deunff@ovhcloud.com>
2025-09-25 15:57:30 +0200

    B0

--- foo/db/models.py
+++ foo/db/models.py
@@ -9,3 +9,16 @@ class DBUser(DBModel):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+
+
+class Group(DBModel):
+    __tablename__ = "groups"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+
+
+user_groups = Table(
+    "user_groups", DBModel.metadata,
+    Column("user_id", ForeignKey("users.id"), primary_key=True),
+    Column("group_id", ForeignKey("groups.id"), primary_key=True)
+)
