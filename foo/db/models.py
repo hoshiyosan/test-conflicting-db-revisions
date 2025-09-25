9e11c1a430824054eed0459ec77c26cd830853a5
Sylvan Le Deunff <sylvan.le-deunff@ovhcloud.com>
2025-09-25 16:39:11 +0200

    A2

--- foo/db/models.py
+++ foo/db/models.py
@@ -9,3 +9,18 @@ class DBUser(DBModel):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+
+
+class DBRole(DBModel):
+    __tablename__ = "roles"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    code: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(150), nullable=True)
+
+
+users_roles = Table(
+    "users_roles", DBModel.metadata,
+    Column("user_id", ForeignKey("users.id"), primary_key=True),
+    Column("role_id", ForeignKey("roles.id"), primary_key=True)
+)
